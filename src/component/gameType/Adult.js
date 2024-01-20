85fe6e0ff1047ff3e22cde1b00ae4ac372dc10af
bhaveshtaparia <bhaveshtaparia7821@gmail.com>
2024-01-20 11:18:29 +0530

    Correction

--- src/component/gameType/Adult.js
+++ src/component/gameType/Adult.js
@@ -5,7 +5,7 @@ function Adult() {
   return (
     <div className='adjust'>
     <Card level={"Chapter-1"} title={"Changer"} description={"Try to talk with Drugs Dealer and try to Change his/her mind"}/>
-    <Card level={"Chapter-2 (Metaverse) "} title={"Virtual Family"} description={"In this Section you will be the part of the Family member of That Hospitalise Person"}/>
+    <Card level={"Chapter-2 (Metaverse) "} title={"Virtual Family"} description={"In this Section you will be the part of the Family member of That Hospitalized Person"}/>
 
     </div>
   )
