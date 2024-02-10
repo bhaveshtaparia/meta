5fd1d1f15185203cbe17e05c4c3fdc3465eeed8c
bhaveshtaparia <bhaveshtaparia7821@gmail.com>
2024-02-10 14:25:28 +0530

    updated navbar

--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -39,7 +39,7 @@ function Navbar() {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="https://drugesc.vercel.app/">Home</Link>
+          <Link className="nav-link active" aria-current="page" to='/'>Home</Link>
         </li>
         <li className="nav-item">
           <Link className="nav-link active" aria-current="page" to="/game">Game</Link>
