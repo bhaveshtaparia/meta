19ff4765a32c16a6a6ccced1dbdf6d7ad1c9d8fa
Himanshusharma1105 <himanshusharma5011@gmail.com>
2024-02-24 17:24:25 +0530

    changed the styling

--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -30,8 +30,8 @@ function Navbar() {
   }
   return (
     <>
-    <nav className="navbar  bg-dark  bg-dark navbar-expand-lg bg-body-tertiary" data-bs-theme="dark" >
-  <div className="container-fluid">
+    <nav className="navbar  bg-dark  bg-dark navbar-expand-lg  bg-body-tertiary" data-bs-theme="dark" >
+  <div className="container-fluid ">
     <img src={require(`../../images/logo.jpeg`)} style={{borderRadius:15}} alt='logo'/>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
